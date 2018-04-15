dd333619c2fe9e8f724cd7182cce4c63e7ce524c
CoE-Warintorn <5810110302@psu.ac.th>
2018-04-15 17:36:46 +0700

    TIMER1 - CTC MODE

---
+++ HW6/TIMER1_C_CTC/TIMER1_C_CTC/main.c
@@ -0,0 +1,29 @@
+/*
+ * TIMER1_C_CTC.c
+ *
+ * Created: 15/4/2561 17:15:58
+ * Author : Warintorn
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+	DDRC	= 0x01;	// set PC0 as output
+	TIMSK1	= 0x02;	// compare match A interrupt enable
+	TCCR1B	= 0x0C;	// No prescaler
+	TCCR1A	= 0x02;	// CTC mode
+	cli();
+	TCNT1	= 0;
+	OCR1A	= 3571;
+	sei();
+	while (1)
+	{
+	}
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTC ^= 0x01;
+}
