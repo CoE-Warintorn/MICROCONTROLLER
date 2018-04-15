3a33c6962fc63ec95f86103223c917a0390a5627
CoE-Warintorn <5810110302@psu.ac.th>
2018-04-15 15:28:50 +0700

    Update Clock Setting

--- HW3/HW3C/HW3C/main.c
+++ HW3/HW3C/HW3C/main.c
@@ -10,7 +10,8 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 
-unsigned char COUNT, TIMER;
+unsigned char COUNT;
+uint16_t TIMER;
 unsigned char LOOKUPTB[] = {
 	0b00111111,	0b00000110, // 0, 1
 	0b01011011,	0b01001111, // 2, 3
@@ -25,29 +26,31 @@ unsigned char LOOKUPTB[] = {
 int main(void)
 {
     DDRB	= 0xFF;
+	PORTB	= LOOKUPTB[0];
 	DDRD	= 0xFB;
 	PORTD	= 0x04;
 	COUNT	= 0;
+	
 	EICRA	= 0x02; // Falling Edge Interrupt
 	EIMSK	= 0x01;	// Enable INT0
 	sei();			// Enable Interrupt
     while (1) 
     {
-		PORTB = LOOKUPTB[COUNT];
+		
     }
 }
 
 ISR(INT0_vect)
 {
 	TIMER = 0;
-	while( !(PIND & 0x04) && TIMER < 187)
+	while( !(PIND & 0x04) && TIMER < 3000)
 	{
 		_delay_ms(1);
 		PORTB ^= 0x80; // Check 3 Sec
 		TIMER++;
 	}
 	
-	if(TIMER==187)
+	if(TIMER==3000)
 	{
 		COUNT = 0;
 	}
@@ -57,4 +60,5 @@ ISR(INT0_vect)
 		if(COUNT==16)
 			COUNT = 0;
 	}
+	PORTB	= LOOKUPTB[COUNT];
 }
