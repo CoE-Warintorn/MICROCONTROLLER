f6fbd58fe2e3e1b28d3e8f49a058c59f30b4b723
CoE-Warintorn <5810110302@psu.ac.th>
2018-05-01 00:28:51 +0700

    First Commit

---
+++ Finalexam/2pulse90diffphase/2pulse90diffphase/main.c
@@ -0,0 +1,38 @@
+/*
+ * 2pulse90diffphase.c
+ *
+ * Created: 1/5/2561 0:07:26
+ * Author : Warintorn
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+	DDRC = 0x03;	// Initial PortC as Output
+	PORTC = 0x00;
+	
+	TCNT1 = 0;
+	OCR1A = 39999;	// Pulse 1
+	OCR1B = 20000;	// Pulse 2 diff 90
+	TCCR1A = 0x00;	// Set CTC mode
+	TCCR1B = 0x0A;	// clock / 8
+	TIMSK1 = 0x06;	// Enable CompA and CompB
+	
+	sei();
+    while (1) 
+    {
+    }
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTC ^= 0x01;
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+	PORTC ^= 0x02;
+}
+
